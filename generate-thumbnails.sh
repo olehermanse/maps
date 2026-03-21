358ac6b70328c6208f8413122c9d4fe7d6f6afd9
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-03-21 20:51:58 +0100

    Better quality thumbnails
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- generate-thumbnails.sh
+++ generate-thumbnails.sh
@@ -10,7 +10,7 @@ mkdir -p "$THUMB_DIR"
 for file in "$PHOTO_DIR"/*.jpg; do
     name="$(basename "$file")"
     echo "Thumbnail $name..."
-    convert "$file" -resize 100x100^ -gravity center -extent 100x100 -quality 80 "$THUMB_DIR/$name"
+    convert "$file" -resize 256x256^ -gravity center -extent 256x256 -quality 80 "$THUMB_DIR/$name"
 done
 
 echo "Done. Generated $(ls "$THUMB_DIR"/*.jpg 2>/dev/null | wc -l) thumbnails."
